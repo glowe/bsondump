077a4344c443e216efe0f2caf97709099ac8aace
glowe <git@spinlag.com>
2020-10-30 12:32:54 -0400

    Make indent more consistent with original tool

--- src/bsondump.rs
+++ src/bsondump.rs
@@ -1,10 +1,12 @@
 use std::io;
 
-const INDENT_SPACES: usize = 4;
-
 pub struct BsonDump<R: io::Read, W: io::Write> {
     reader: R,
     writer: W,
+}
+
+fn get_indent(indent_level: usize) -> String {
+    " ".repeat(indent_level * 4)
 }
 
 impl<R, W> BsonDump<R, W>
@@ -46,31 +48,24 @@ where
         Ok(())
     }
 
-    fn debug_array(&mut self, elements: &Vec<bson::Bson>, indent: usize) -> io::Result<u32> {
+    fn debug_array(&mut self, elements: &Vec<bson::Bson>, indent_level: usize) -> io::Result<u32> {
         let mut num_objects = 0;
-        write!(
-            &mut self.writer,
-            "{spacer:indent$}--- new object ---\n",
-            spacer = " ",
-            indent = indent * INDENT_SPACES
-        )?;
-        let indent = indent + 1;
+        write!(&mut self.writer, "{}--- new object ---\n", get_indent(indent_level))?;
         for element in elements {
             // We can't get size without raw bson, but the bson crate doesn't support raw bson yet.
             write!(
                 &mut self.writer,
-                "{spacer:indent$}type: {type}\n",
-                spacer=" ",
-                indent=indent * INDENT_SPACES,
+                "{indent}type: {type}\n",
+                indent=get_indent(indent_level + 2),
                 type=element.element_type() as u8,
             )?;
             num_objects += 1;
             match element {
                 bson::Bson::Document(inner) => {
-                    num_objects += self.debug_document(&inner, indent + 1)?;
+                    num_objects += self.debug_document(&inner, indent_level + 3)?;
                 }
                 bson::Bson::Array(inner) => {
-                    num_objects += self.debug_array(&inner, indent + 1)?;
+                    num_objects += self.debug_array(&inner, indent_level + 3)?;
                 }
                 _ => {}
             }
@@ -78,32 +73,34 @@ where
         Ok(num_objects)
     }
 
-    fn debug_document(&mut self, document: &bson::Document, indent: usize) -> io::Result<u32> {
+    fn debug_document(
+        &mut self,
+        document: &bson::Document,
+        indent_level: usize,
+    ) -> io::Result<u32> {
         let mut num_objects = 0;
-        write!(
-            &mut self.writer,
-            "{spacer:indent$}--- new object ---\n",
-            spacer = " ",
-            indent = indent * INDENT_SPACES
-        )?;
+        write!(&mut self.writer, "{}--- new object ---\n", get_indent(indent_level))?;
         for (name, element) in document {
             // We can't get size without raw bson, but the bson crate doesn't support raw bson yet.
             write!(
                 &mut self.writer,
-                "{spacer:indent$}{name}\n{spacer:double_indent$}type: {type}\n",
-                spacer=" ",
-                indent=indent * INDENT_SPACES,
-                name=name,
-                double_indent=(indent + 1) * INDENT_SPACES,
+                "{indent}{name}\n",
+                indent = get_indent(indent_level + 1),
+                name = name,
+            )?;
+            write!(
+                &mut self.writer,
+                "{indent}type: {type}\n",
+                indent=get_indent(indent_level + 2),
                 type=element.element_type() as u8,
             )?;
             num_objects += 1;
             match element {
                 bson::Bson::Document(inner) => {
-                    num_objects += self.debug_document(inner, indent + 1)?;
+                    num_objects += self.debug_document(inner, indent_level + 3)?;
                 }
                 bson::Bson::Array(inner) => {
-                    num_objects += self.debug_array(inner, indent + 1)?;
+                    num_objects += self.debug_array(inner, indent_level + 3)?;
                 }
                 _ => {}
             }
