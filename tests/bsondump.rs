1464ea352ee48c4074570c1a1b8356d4d507027e
Graham Lowe <588088+glowe@users.noreply.github.com>
2022-07-22 19:15:09 -0400

    Implement 2 tests

--- tests/bsondump.rs
+++ tests/bsondump.rs
@@ -1,31 +1,62 @@
 mod tests {
-    use std::io::Write;
+    use std::io::{Read, Write};
     use std::process::Stdio;
 
+    use tempfile::NamedTempFile;
+
     use test_bin;
+
+    const SAMPLE_BSON: &[u8; 283] = include_bytes!("testdata/sample.bson");
+    const SAMPLE_JSON: &[u8; 575] = include_bytes!("testdata/sample.json");
 
     #[test]
     fn from_stdin_to_stdout() {
         let mut child = test_bin::get_test_bin("bsondump")
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
             .spawn()
             .expect("failed to spawn process");
+
         let mut stdin = child.stdin.take().expect("Failed to open stdin");
-        let sample_bson = include_bytes!("testdata/sample.bson");
+
         std::thread::spawn(move || {
-            stdin.write_all(sample_bson).expect("Failed to write to stdin");
+            stdin
+                .write_all(SAMPLE_BSON)
+                .expect("Failed to write to stdin");
         });
 
-        let sample_json = include_str!("testdata/sample.json");
         let output = child.wait_with_output().expect("Failed to read stdout");
-        assert_eq!(String::from_utf8_lossy(&output.stdout), sample_json);
-        assert!(String::from_utf8_lossy(&output.stderr).ends_with("4 objects found\n"));
+        assert_eq!(&output.stdout, SAMPLE_JSON);
     }
 
     #[test]
-    fn from_stdin_to_file() {}
+    fn from_stdin_to_file() {
+        let out_file = NamedTempFile::new().expect("Failed to create temporary out file");
+
+        let mut child = test_bin::get_test_bin("bsondump")
+            .args([
+                "--outFile",
+                out_file.path().to_str().expect("Failed get path"),
+            ])
+            .stdin(Stdio::piped())
+            .spawn()
+            .expect("failed to spawn process");
+
+        let mut stdin = child.stdin.take().expect("Failed to open stdin");
+
+        std::thread::spawn(move || {
+            stdin
+                .write_all(SAMPLE_BSON)
+                .expect("Failed to write to stdin");
+        });
+
+        child.wait().expect("Failed to write");
+
+        let mut file = std::fs::File::open(out_file.path()).expect("Failed to open out file");
+        let mut buf: Vec<u8> = Vec::new();
+        file.read_to_end(&mut buf).expect("Failed to read out file");
+        assert_eq!(buf, SAMPLE_JSON);
+    }
 
     #[test]
     fn from_file_with_named_argument_to_stdout() {}
